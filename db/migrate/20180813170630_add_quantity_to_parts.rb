9dfbcb8c1bc97798ec3ea41891ba6cf22bfcd990
Billy Davidson <billy.davids52@gmail.com>
2018-08-13 14:50:59 -0400

    rename column

--- db/migrate/20180813170630_add_quantity_to_parts.rb
+++ db/migrate/20180813170630_add_quantity_to_parts.rb
@@ -4,4 +4,15 @@ class AddQuantityToParts < ActiveRecord::Migration[5.2]
       t.integer :quantity
     end
   end
+
+#   def up
+#     change_table :parts do |t|
+#       t.decimal :quantity
+#     end
+#     rename_column :parts, :quantity, :decimal
+#   end
+#
+#   def down
+#     rename_column :users, :quantity, :decimal
+#   end
 end
